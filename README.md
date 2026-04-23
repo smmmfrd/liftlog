fca5045378dd9e3564e68a9696b2704426ed94bc
Sam Mumford <smmmfrd@gmail.com>
2026-04-22 23:27:03 -0700

    Removed Boiler Plate Vite and Fix Vite Refactor

---
+++ README.md
@@ -0,0 +1,7 @@
+# Liftlog
+
+An app to track your reps and weight on exercises you set. Will eventually also have graphs and such to show your progress.
+
+## Vite
+
+This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "./assets/vite.svg";
+import heroImg from "./assets/hero.png";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <section id="center">
+        <h1>Get started</h1>
+        <div className="hero">
+          <img src={heroImg} className="base" width="170" height="179" alt="" />
+          <img src={reactLogo} className="framework" alt="React logo" />
+          <img src={viteLogo} className="vite" alt="Vite logo" />
+        </div>
+        <button
+          type="button"
+          className="counter"
+          onClick={() => setCount((count) => count + 1)}
+        >
+          Count is {count}
+        </button>
+      </section>
+
+      <div className="ticks">{count}</div>
+    </>
+  );
+}
+
+export default App;
