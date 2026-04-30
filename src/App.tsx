ef8270acb70e0bf90c20efa8b09dc650331e388b
Sam Mumford <smmmfrd@gmail.com>
2026-04-29 19:41:18 -0700

    Add Mantine UI

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "./assets/vite.svg";
 import heroImg from "./assets/hero.png";
+import { Button } from "@mantine/core";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -15,13 +16,9 @@ function App() {
           <img src={reactLogo} className="framework" alt="React logo" />
           <img src={viteLogo} className="vite" alt="Vite logo" />
         </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
+        <Button onClick={() => setCount((count) => count + 1)}>
           Count is {count}
-        </button>
+        </Button>
       </section>
 
       <div className="ticks">{count}</div>
